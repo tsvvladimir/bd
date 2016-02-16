23a512fe6ed9e0ca60dbddf86d4114f8596e5902
Vladimir Tsvetkov <tsvvladimir95@gmail.com>
2016-02-16 13:15:07 +0300

    add replication

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from collections import defaultdict
 import pickle
+from time import sleep
 
 class DB:
     def __init__(self):
         self.db = defaultdict(list)
 
-    def create(self, k, v):
-        self.db[k] = [v]
-        print "create success"
+    def enable_repl(self):
+        self.db["complete"] = 1
+        self.savetofile("db1_repl")
+        self.savetofile("db2_repl")
 
     def read_u(self, k):
         if not self.db[k]:
@@ -17,23 +19,212 @@ class DB:
 
     def read_s(self, k):
         try:
-            print db.read_u(k)
+            print self.read_u(k)
         except KeyError as e:
             print e
+
+    def read_s_repl(self, k):
+        self.no_change_repl_2(k, lambda dbs, k: dbs.read_s(k))
+
+    def no_change_repl_2(self, k, lbd):
+        db1 = DB()
+        db2 = DB()
+        db1_flag = False
+        db2_flag = False
+        try:
+            db1.loadfromfile("db1_repl")
+            #print db1.db
+        except IOError as e:
+            print "db recovered"
+        else:
+            db1_flag = True
+        try:
+            db2.loadfromfile("db2_repl")
+            #print db1.db
+        except IOError as e:
+            print "db recovered"
+        else:
+            db2_flag = True
+        done1 = False
+        done2 = False
+        if db1_flag:
+            if db1.db["complete"] == 1:
+                lbd(db1, k)
+                done1 = True
+        if (not done1) and db2_flag:
+            if db2.db["complete"] == 1:
+                lbd(db2, k)
+                done2 = True
+        if (not done1) and (not done2):
+            raise MemoryError("db corrupted")
+
+    def create(self, k, v):
+        self.db[k] = [v]
+        print "create success"
+
+    def create_repl(self, k, v):
+        self.change_repl_3(k, v, lambda dbs, k, v: dbs.create(k, v))
 
     def update(self, k, v):
         self.db[k].append(v)
         print "update sucess"
 
+    def update_repl(self, k, v):
+        self.change_repl_3(k, v, lambda dbs, k, v: dbs.update(k, v))
+
+    def change_repl_3(self, k, v, lbd):
+        db1 = DB()
+        db2 = DB()
+        db1_flag = False
+        db2_flag = False
+        try:
+            db1.loadfromfile("db1_repl")
+        except IOError as e:
+            print "db recovered"
+        else:
+            db1_flag = True
+        try:
+            db2.loadfromfile("db2_repl")
+        except IOError as e:
+            print "db recovered"
+        else:
+            db2_flag = True
+        done1 = False
+        done2 = False
+        if db1_flag:
+            if db1.db["complete"] == 1:
+                #db1.db[k] = [v]
+                lbd(db1, k, v)
+                done1 = True
+        if (not done1) and db2_flag:
+            if db2.db["complete"] == 1:
+                #db2.db[k] = [v]
+                lbd(db1, k, v)
+                done2 = True
+        if (not done1) and (not done2):
+            raise MemoryError("db corrupted")
+        if done1:
+            db1.savetofile("db1_repl")
+            db1.savetofile("db2_repl")
+        if done2:
+            db2.savetofile("db1_repl")
+            db2.savetofile("db2_repl")
+        #print "create success"
+
     def delete(self, k):
         self.db[k] = list()
         print "delete success"
 
+    def delete_repl(self, k):
+        self.change_repl_2(k, lambda dbs, k: dbs.delete(k))
+
+    def change_repl_2(self, k, lbd):
+        db1 = DB()
+        db2 = DB()
+        db1_flag = False
+        db2_flag = False
+        try:
+            db1.loadfromfile("db1_repl")
+        except IOError as e:
+            print "db recovered"
+        else:
+            db1_flag = True
+        try:
+            db2.loadfromfile("db2_repl")
+        except IOError as e:
+            print "db recovered"
+        else:
+            db2_flag = True
+        done1 = False
+        done2 = False
+        if db1_flag:
+            if db1.db["complete"] == 1:
+                #db1.db[k] = list()
+                lbd(db1, k)
+                done1 = True
+        if (not done1) and db2_flag:
+            if db2.db["complete"] == 1:
+                #db2.db[k] = list()
+                lbd(db2, k)
+                done2 = True
+        if (not done1) and (not done2):
+            raise MemoryError("db corrupted")
+        if done1:
+            db1.savetofile("db1_repl")
+            db1.savetofile("db2_repl")
+        if done2:
+            db2.savetofile("db1_repl")
+            db2.savetofile("db2_repl")
+        #print "delete success"
+
     def savetofile(self, filename):
         pickle.dump(self.db, open(filename, "wb"))
+        #print "save success"
 
     def loadfromfile(self, filename):
         self.db = pickle.load(open(filename, "rb"))
+        #print "load success"
+
+    def savetofile_repl(self, filename):
+        #pickle.dump(self.db, open(filename, "wb"))
+        db1 = DB()
+        db2 = DB()
+        db1_flag = False
+        db2_flag = False
+        try:
+            db1.loadfromfile("db1_repl")
+            #print db1.db
+        except IOError as e:
+            print "db recovered"
+        else:
+            db1_flag = True
+        try:
+            db2.loadfromfile("db2_repl")
+            #print db1.db
+        except IOError as e:
+            print "db recovered"
+        else:
+            db2_flag = True
+        done1 = False
+        done2 = False
+        if db1_flag:
+            if db1.db["complete"] == 1:
+                pickle.dump(self.db, open(filename, "wb"))
+                db1.savetofile(filename)
+                #db1.savetofile("db2_repl")
+                done1 = True
+        if (not done1) and db2_flag:
+            if db2.db["complete"] == 1:
+                pickle.dump(self.db, open(filename, "wb"))
+                db2.savetofile(filename)
+                #db2.savetofile("db2_repl")
+                done2 = True
+        if (not done1) and (not done2):
+            raise MemoryError("db corrupted")
+        print "save success"
+
+    def loadfromfile_repl(self, filename):
+        db1 = DB()
+        db1_flag = False
+        try:
+            db1.loadfromfile(filename)
+            #print db1.db
+        except IOError as e:
+            raise e
+        else:
+            db1_flag = True
+        done1 = False
+        if db1_flag:
+            if db1.db["complete"] == 1:
+                self.db = pickle.load(open(filename, "rb"))
+                self.savetofile("db1_repl")
+                self.savetofile("db2_repl")
+                done1 = True
+        if not done1:
+            raise MemoryError("db corrupted")
+        print "load success"
+
+
 
 DB_API_func_2 = {
     "read": lambda dbs, k: dbs.read_s(k) ,
@@ -47,39 +238,67 @@ DB_API_func_3 = {
     "update": lambda dbs, k, v: dbs.update(k, v),
 }
 
+DB_API_func_2_repl = {
+    "read": lambda dbs, k: dbs.read_s_repl(k) ,
+    "delete": lambda dbs, k: dbs.delete_repl(k),
+    "savetofile": lambda dbs, fname: dbs.savetofile_repl(fname),
+    "loadfromfile": lambda dbs, fname: dbs.loadfromfile_repl(fname)
+}
+
+DB_API_func_3_repl = {
+    "create": lambda dbs, k, v: dbs.create_repl(k, v),
+    "update": lambda dbs, k, v: dbs.update_repl(k, v),
+}
+
 db = DB()
+db.enable_repl()
+
+#standard target
 '''
-db.create("k1", 1)
+db.create_repl("k1", 1)
 
-print db.read_s("k1")
-print db.read_s("k2")
+db.read_s_repl("k1")
+db.read_s_repl("k2")
 
-db.create("k1", 2)
+db.create_repl("k1", 2)
 
-print db.read_s("k1")
+db.read_s_repl("k1")
 
-db.update("k2", 3)
-db.update("k2", 4)
+db.update_repl("k2", 3)
+db.update_repl("k2", 4)
 
-print db.read_s("k2")
+db.read_s_repl("k2")
 
-db.delete("k1")
+db.delete_repl("k1")
 
-print db.read_s("k1")
+db.read_s_repl("k1")
 
-db.savetofile("mydb")
+db.savetofile_repl("mydb")
 
-db.update("k1", 1)
+db.update_repl("k1", 666)
 
-db.loadfromfile("mydb")
+db.loadfromfile_repl("mydb")
 
-db.read_s("k1")
+db.read_s_repl("k1")
 '''
+#load save target
+'''
+db.create_repl("k1", 333)
+
+db.savetofile_repl("mydb")
+
+db.create_repl("k1", 666)
+
+db.loadfromfile_repl("mydb")
+
+db.read_s_repl("k1")
+'''
+
 while True:
     command = raw_input().split()
     if command[0] == "exit":
         exit()
     if len(command) == 2:
-        DB_API_func_2[command[0]](db, command[1])
+        DB_API_func_2_repl[command[0]](db, command[1])
     elif len(command) == 3:
-        DB_API_func_3[command[0]](db, command[1], command[2])
+        DB_API_func_3_repl[command[0]](db, command[1], command[2])
